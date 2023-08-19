fb46bfe427dce5313878b1851cb9ac8fb4e4c6e6
sami <samyramadan.bts@gmail.com>
2023-08-19 13:37:35 +0300

    if statement

---
+++ dist/7-if-Statement.js
@@ -0,0 +1,6 @@
+"use strict";
+let num1 = 10;
+let num2 = 5;
+let num3 = 15;
+console.log(num1 > num2 ? `${num1} is greater than ${num2}` : `${num1} is less than ${num2}`);
+console.log(num1 > num3 ? `${num1} is greater than ${num3}` : `${num1} is less than ${num3}`);

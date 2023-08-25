26882dcc5bb7f3149f2ba771c55691b936ab3511
sami <samyramadan.bts@gmail.com>
2023-08-25 22:02:31 +0300

    Recursive Types

---
+++ src/16-Recursive-Types.ts
@@ -0,0 +1,7 @@
+type NestedArray = number | NestedArray[];
+
+// const numberss: NestedArray = [1, 2];
+// const numberss: NestedArray = [1, [2, 3, 4]];
+// const numberss: NestedArray = [1, [2, 3, [4]]];
+// const numberss: NestedArray = [1, [2, 3, [4, 5, [6]]]];
+// const numberss: NestedArray = [1, [2, 3, [4, 5, [6, 7, [8]]]]];

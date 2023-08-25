7ebd2766f7ade68e1f9d503ea8efd4481a44c768
sami <samyramadan.bts@gmail.com>
2023-08-25 21:55:21 +0300

    fix types

---
+++ src/15-Type-elias.ts
@@ -0,0 +1,33 @@
+// type CourseDiscounts = number;
+
+// let courseDiscounts: CourseDiscounts = 25;
+// courseDiscounts = 5;
+
+// ---------------------------------------
+
+// type CourseDiscounts = 25 | 35 | 45 | 75
+
+// let courseDiscounts: CourseDiscounts = 35;
+
+// ---------------------------------------
+
+// type TSCourse = "TS";
+
+// let coursName: TSCourse = "TS";
+
+// ---------------------------------------
+
+// type MyCourses = {
+//   name: string;
+//   price: number;
+// }
+
+// const tsCourses: MyCourses = {
+//   name: "TS Bootcamp",
+//   price: 10.99
+// }
+
+// const jsCourses: MyCourses = {
+//   name: "JS Bootcamp",
+//   price: 20.99,
+// }

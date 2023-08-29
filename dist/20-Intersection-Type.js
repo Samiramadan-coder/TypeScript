11f2a0e786cd5da89071f4b1c0e3410b71d11c0d
sami <samyramadan.bts@gmail.com>
2023-08-29 21:38:39 +0300

    Intersection Type

---
+++ dist/20-Intersection-Type.js
@@ -0,0 +1,25 @@
+"use strict";
+const alanWrench = {
+    id: 1,
+    category: "Hardware",
+    name: "Alan Wrench",
+    price: 12.99,
+    stock: 0,
+    discount: () => { },
+    holidaySales: () => { },
+    subscription: () => { },
+    shipping: () => { }
+};
+const tapMeasure = {
+    id: 2,
+    category: "Hardware",
+    name: "Tap Measure",
+    price: 2.99,
+    stock: 23,
+    discount: () => { },
+    holidaySales: () => { },
+    subscription: () => { },
+    shipping: () => { }
+};
+console.log(alanWrench);
+console.log(tapMeasure);
---
+++ src/20-Intersection-Type.ts
@@ -0,0 +1,58 @@
+type ProductProperties = {
+  id: number;
+  category: string;
+  name: string;
+  price: number;
+  stock: number;
+}
+
+type ProductDiscounts = {
+  discount(): void;
+}
+
+type ProductSeasonalSales = {
+  holidaySales(): void;
+}
+
+type ProductSubscription = {
+  subscription(): void;
+}
+
+type ProductShipping = {
+  shipping(): void;
+}
+
+type Product = ProductProperties & 
+  ProductDiscounts & 
+  ProductSeasonalSales & 
+  ProductSubscription & 
+  ProductShipping;
+
+const alanWrench: Product = {
+  id: 1,
+  category: "Hardware",
+  name: "Alan Wrench",
+  price: 12.99,
+  stock: 0,
+
+  discount: () => {},
+  holidaySales: () => {},
+  subscription: () => {},
+  shipping: () => {}
+}
+
+const tapMeasure: Product = {
+  id: 2,
+  category: "Hardware",
+  name: "Tap Measure",
+  price: 2.99,
+  stock: 23,
+
+  discount: () => {},
+  holidaySales: () => {},
+  subscription: () => {},
+  shipping: () => {}
+}
+
+console.log(alanWrench);
+console.log(tapMeasure);

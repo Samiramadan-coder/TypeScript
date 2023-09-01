09407cf44ee1138968c167b65e9489648b79fa0c
sami <samyramadan.bts@gmail.com>
2023-09-01 11:43:59 +0300

    The Object Type

---
+++ src/22-The-Object-Type.ts
@@ -0,0 +1,35 @@
+// type Product = {
+//   name: string;
+//   price: number;
+//   readonly level: string;
+//   released?: boolean;
+// }
+
+// const product: Product = {
+//   name: "TS",
+//   price: 10.99,
+//   level: "Beginner",
+//   released: true,
+// };
+
+// product.released = true;
+// console.log(product);
+
+// let onlineCourse: Product = {
+//   name: "JS",
+//   price: 9.99,
+//   level: "Beginner",
+// }
+
+// --------------------------------------------
+
+// type Student = { fullName: string };
+
+// function studentDB({ fullName }: Student): string {
+//   return fullName;
+// }
+
+// const student: Student = { fullName: "Sami Ramadan" };
+
+// console.log(studentDB(student));
+// console.log(studentDB({fullName: "Amr Sami"}));

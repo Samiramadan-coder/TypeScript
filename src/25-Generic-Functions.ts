ec13fe31f53f87ab5a0a2a93674fd260799fcd4b
sami <samyramadan.bts@gmail.com>
2023-09-02 14:22:47 +0300

    Generic Functions

---
+++ src/25-Generic-Functions.ts
@@ -0,0 +1,10 @@
+// function getArrayItem<T>(item: Array<T>): T {
+//   return item[2];
+// }
+
+// let number = getArrayItem<number>([1, 2, 3, 4]);
+// console.log(number);
+
+// let string = getArrayItem<string>(["Hi", "Hey", "Hello", "There"]);
+// console.log(string);
+
